ea1ed2a30cd252722fe5a818a8280f7be4e50fe5
Jamila.C <144802549+JamilaCherkaoui@users.noreply.github.com>
2024-11-29 15:17:16 +0100

    Ajout pages

---
+++ Welcome.py
@@ -0,0 +1,271 @@
+import streamlit as st
+import requests
+import time
+
+# Configuration de la page
+st.set_page_config(
+    page_title="この旅",
+    page_icon="👋",
+    layout="wide"
+)
+img_path = "./images/welcome.png" 
+# Display the image at the top of the page
+st.image(img_path, use_column_width=True)
+
+# Initialize the language state if it doesn't exist
+if 'language' not in st.session_state:
+    st.session_state['language'] = 'English'
+
+if st.sidebar.button("Switch to Japanese" if st.session_state['language'] == 'English' else "Switch to English"):
+    # Toggle the language
+    st.session_state['language'] = 'Japanese' if st.session_state['language'] == 'English' else 'English'
+
+# Display content based on the current language
+if st.session_state['language'] == 'English':
+    st.sidebar.title("Welcome to the StreamiManga App! 🎥🎌")
+    st.markdown("""
+    Dive into the world of anime where data meets entertainment! 🚀🎌  
+    We are students passionate about anime who created this site to share our love for this genre and analyze viewing trends.  
+    - 📊 Explore viewership statistics and trends.  
+    - 🤖 Get AI-powered recommendations tailored to your taste.  
+    - 🎮 Test your anime knowledge with a fun quiz.  
+    - 📺 Discover the most popular shows currently trending.  
+    - 🎨 Generate unique anime characters using advanced diffusion models.  
+
+    Join us and uncover the magic of anime! 💖✨
+    """)
+else:
+    st.sidebar.title("StreamiManga アプリへようこそ！ 🎥🎌")
+    st.markdown("""
+    アニメの世界へようこそ！ここではデータとエンターテインメントが融合します！🚀🎌  
+    私たちはアニメに情熱を持つ学生で、このサイトを作り、このジャンルへの愛を共有し、視聴傾向を分析しています。  
+    - 📊 視聴者統計とトレンドを探る。  
+    - 🤖 あなたの好みに合わせたAIによるおすすめをゲット。  
+    - 🎮 楽しいクイズでアニメの知識をテスト。  
+    - 📺 現在話題の人気番組を発見。  
+    - 🎨 高度な拡散モデルを使用してユニークなアニメキャラクターを生成。  
+
+    アニメの魔法を一緒に体験しましょう！💖✨
+    """)
+
+#st.sidebar.title("✨ Welcome to the StreamiManga App! 🎥🌟")
+logo_path = "./images/streami.png"  
+# Display the logo image in the sidebar
+st.sidebar.image(logo_path, use_column_width=True)
+
+
+# Fonction pour récupérer les animés top
+@st.cache_data(ttl=3600)  # Cache les données pendant 1 heure
+def get_top_anime():
+    response = requests.get("https://api.jikan.moe/v4/top/anime")
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']
+    else:
+        return []
+
+# Récupération des animés top
+top_anime = get_top_anime()
+
+# Filtrer pour garder uniquement les animés avec un trailer
+top_anime_with_trailers = [anime for anime in top_anime if anime.get('trailer', {}).get('embed_url')]
+
+# Vérification si les données ont été récupérées
+if not top_anime_with_trailers:
+    st.error("Could not get popular animes with trailers.")
+else:
+    # Initialiser l'index de l'animation dans st.session_state s'il n'existe pas
+    if 'anime_index' not in st.session_state:
+        st.session_state['anime_index'] = 0
+
+    st.title("Most Popular Animes at the moment")
+    carousel_placeholder = st.empty()
+
+    # CSS personnalisé
+    st.markdown(
+        """
+        <style>
+        .anime-card {
+            display: flex;
+            border: 1px solid #333;
+            border-radius: 10px;
+            padding: 15px;
+            margin-top: 20px;
+            gap: 15px;
+            flex-wrap: wrap;
+            transition: transform 0.1s ease-out;
+            transform: perspective(1000px);
+            will-change: transform;
+        }
+
+        
+
+        .anime-image-container {
+            position: relative;
+            width: 300px;
+            border-radius: 5px;
+            flex: 0 0 300px;
+            overflow: hidden;
+        }
+        .anime-image {
+            width: 100%;
+            border-radius: 5px;
+        }
+        .anime-overlay {
+            position: absolute;
+            bottom: 0;
+            left: 0;
+            width: 100%;
+            height: 50px;
+            background: linear-gradient(transparent, rgba(0, 0, 0, 0.7));
+        }
+        .anime-rating {
+            position: absolute;
+            bottom: 10px;
+            left: 10px;
+            padding: 3px 8px;
+            font-size: 10px;
+            color: #fff;
+        }
+        .anime-details {
+            flex: 2;
+        }
+        .anime-right-section {
+            flex: 3;
+            display: flex;
+            flex-direction: column;
+            justify-content: flex-start;
+        }
+        .anime-header {
+            padding: 3px 8px;
+            border: 1px solid #333;
+            border-radius: 5px;
+            display: inline-block;
+            font-size: 14px;
+            margin-bottom: 10px;
+        }
+        .anime-title {
+            font-size: 28px;
+            margin: 10px 0;
+            font-weight: bold;
+        }
+        .anime-meta {
+            font-size: 14px;
+            margin: 5px 0;
+        }
+        .anime-stats {
+            display: flex;
+            align-items: center;
+            margin-top: 10px;
+        }
+        .anime-stats div {
+            margin-right: 15px;
+            display: flex;
+            align-items: center;
+        }
+        .anime-stats div span {
+            font-size: 15px;
+            font-weight: bold;
+            margin-right: 5px;
+        }
+        .tag {
+            display: inline-block;
+            background-color: #292928;
+            padding: 3px 8px;
+            border-radius: 5px;
+            font-size: 12px;
+            margin-right: 5px;
+            color: #fff;
+            text-decoration: none;
+        }
+        .tag a {
+            color: inherit; /* This ensures the link takes the color of its parent */
+            text-decoration: none; /* Removes underline */
+        }
+
+        .tag:hover {
+            background-color: #444;
+        }
+
+        .tag a:hover {
+            color: inherit; /* Keeps the hover color the same as the tag */
+        }
+        iframe {
+            margin-top: 10px;
+            border-radius: 5px;
+            width: 100%;
+            height: 250px; /* Augmenter la hauteur pour une meilleure visibilité */
+        }
+        .synopsis {
+            margin-top: 15px;
+            font-size: 15px;
+            line-height: 1.5;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
+    # Boucle infinie pour le carrousel
+    while True:
+        with carousel_placeholder.container():
+            # Afficher les détails de l'animé sélectionné
+            selected_anime = top_anime_with_trailers[st.session_state['anime_index']]
+            title = selected_anime['title']
+            image_url = selected_anime['images']['jpg']['large_image_url']
+            score = selected_anime.get('score', 'N/A')
+            rank = selected_anime.get('rank', 'N/A')
+            users = selected_anime.get('scored_by', 'N/A')
+            status = selected_anime.get('status', 'N/A')
+            start_date = selected_anime.get('aired', {}).get('string', 'N/A')
+            episodes = selected_anime.get('episodes', 'N/A')
+            genres = selected_anime.get('genres', [])
+            trailer_url = selected_anime['trailer']['embed_url']
+            synopsis = selected_anime.get('synopsis', 'Synopsis not available.')
+            rating = selected_anime.get('rating', 'N/A')
+
+            status_color = "lightblue" if status == "Finished Airing" else "lightgreen" if status == "Currently Airing" else "orange"
+
+            # Affichage des détails de l'animé
+            st.markdown(
+                f"""
+                <div class="anime-card">
+                    <div class="anime-image-container">
+                        <img src="{image_url}" class="anime-image">
+                        <div class="anime-overlay"></div>
+                        <div class="anime-rating">{rating}</div>
+                    </div>
+                    <div class="anime-details">
+                        <div class="anime-header" style="color: {status_color};">{status}</div>
+                        <h2 class="anime-title">{title}</h2>
+                        <div class="anime-stats">
+                            <div><span>⭐</span> {score}</div>
+                            <div>{users} users</div>
+                            <div># {rank} Ranking</div>
+                        </div>
+                        <div class="anime-meta">
+                            Start Date: {start_date} | Episodes: {episodes}
+                        </div>
+                        <div>
+                            {" ".join([
+                                f'<a href="{genre["url"]}" target="_blank" class="tag" style="color: #fff; text-decoration: none;">{genre["name"]}</a>' 
+                                for genre in genres
+                            ])}
+                        </div>
+                    </div>
+                    <div class="anime-right-section">
+                        <iframe src="{trailer_url}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                                                <p class="synopsis"> {synopsis[:300]}...</p>
+                    </div>
+                </div>
+                """,
+                unsafe_allow_html=True
+            )
+
+        # Incrémenter l'index pour le prochain élément du carrousel
+        st.session_state['anime_index'] = (st.session_state['anime_index'] + 1) % len(top_anime_with_trailers)
+        
+        # Pause de 18 secondes pour l'affichage de chaque animé
+        time.sleep(6)
+
+
---
+++ pages/1_📺_Who_Watches_Animes_.py
@@ -0,0 +1,220 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+
+st.set_page_config(page_title="Who Watches Animes ?", page_icon="📺", layout="wide")
+
+st.markdown("# Who Watches Animes アニメを見るのは誰 ？ 📺")
+st.sidebar.header("Who Are Anime Fans? 🤔")
+
+# Cache for loading the logo
+@st.cache_resource
+def load_logo():
+    return "./images/streami.png"
+
+logo_path = load_logo()
+st.sidebar.image(logo_path, use_column_width=True)
+
+# Cache for loading datasets
+@st.cache_data
+def load_data():
+    anime_data_path = './data/anime/anime-dataset-2023.csv'
+    user_details_path = './data/user/users-details-2023.csv'
+    user_scores_path = './data/user/user_scores_filtered.csv'
+    anime_filtered_path = './data/anime/anime-filtered.csv'
+
+    anime_df = pd.read_csv(anime_data_path)
+    user_details_df = pd.read_csv(user_details_path)
+    user_scores_df = pd.read_csv(user_scores_path)
+    anime_filtered_df = pd.read_csv(anime_filtered_path)
+    return anime_df, user_details_df, user_scores_df, anime_filtered_df
+
+anime_df, user_details_df, user_scores_df, anime_filtered_df = load_data()
+
+# Convert the Score column to numeric values, handling errors
+anime_df['Score'] = pd.to_numeric(anime_df['Score'], errors='coerce')
+
+# Creating tabs
+tab1, tab2, tab3 = st.tabs(["Explanation", "General Overview", "Anime Selection"])
+
+with tab1:
+    st.subheader("About This Page")
+    st.markdown("""
+    Welcome to the **Who Watches Animes?** dashboard! This page provides insights into anime viewership and allows you to explore detailed statistics and user data. Here's a quick overview of what you'll find in the different tabs:
+
+    - **General Overview**: This tab presents a summary of various anime statistics, including the distribution of different types of anime and the gender distribution among viewers. You can also explore the average episodes watched and average days spent watching anime, categorized by gender.
+    
+    - **Anime Selection**: This tab allows you to select a specific anime and view detailed information about it, such as its popularity, score, members, and completion statistics. You'll also see the average rating given by male and female viewers and a pie chart showing the gender distribution of viewers who have rated the anime.
+    
+    Enjoy exploring the data and learning more about anime viewership! 🎉
+    """)
+
+with tab2:
+    st.subheader("General Overview")
+
+    # Cache computation of the average score
+    @st.cache_data
+    def calculate_average_score(dataframe):
+        return dataframe['Score'].mean()
+
+    average_score = calculate_average_score(anime_df)
+
+    # CSS for styling and animation
+    st.markdown("""
+        <style>
+        .average-score {
+            display: flex;
+            justify-content: flex-end;
+            align-items: center;
+            font-size: 2em;
+            font-weight: bold;
+        }
+        .average-score .score-value, .average-score .star {
+        margin-left: 10px;
+        animation: bounce 1s infinite;
+        }
+        @keyframes bounce {
+            0%, 20%, 50%, 80%, 100% {
+                transform: translateY(0);
+            }
+            40% {
+                transform: translateY(-10px);
+            }
+            60% {
+                transform: translateY(-5px);
+            }
+        }
+        .description {
+            font-size: 0.8em;
+            margin-right: 10px;
+        }
+        </style>
+    """, unsafe_allow_html=True)
+
+    st.markdown(
+        f"""
+        <div class="average-score">
+            <span class="description">The user's average score for an anime is</span>
+            <span class="score-value">{average_score:.2f} /10</span>
+            <span class="star">⭐ !</span>
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
+
+    # Cache computation of anime type counts
+    @st.cache_data
+    def compute_type_counts(dataframe):
+        dataframe['Type'] = dataframe['Genres'].apply(lambda x: x.split(',')[0] if pd.notnull(x) else 'Unknown')
+        counts = dataframe['Type'].value_counts()
+        return counts[counts.index != 'UNKNOWN']  # Remove 'Unknown'
+
+    type_counts = compute_type_counts(anime_df)
+
+    fig_bar = px.bar(
+        type_counts,
+        x=type_counts.index,
+        y=type_counts.values,
+        title="Distribution of Anime Types",
+        labels={'x': 'Type', 'y': 'Count'},
+        template="plotly_dark"
+    )
+    fig_bar.update_layout(
+        plot_bgcolor='#0f1116',
+        paper_bgcolor='#0f1116',
+        font_color='white'
+    )
+
+    gender_counts = user_details_df['Gender'].value_counts()
+    fig_pie = go.Figure(data=[go.Pie(
+        labels=gender_counts.index,
+        values=gender_counts.values,
+        hoverinfo='label+percent+value',
+        marker=dict(colors=['bluepink', 'pink'])
+    )])
+    fig_pie.update_layout(
+        title="Gender distribution among viewers",
+        template="plotly_dark",
+        plot_bgcolor='#0f1116',
+        paper_bgcolor='#0f1116',
+        font_color='white'
+    )
+
+    col1, spacer, col2 = st.columns([2, 1, 2])
+    with col1:
+        st.plotly_chart(fig_bar, use_container_width=True)
+    with col2:
+        st.plotly_chart(fig_pie, use_container_width=True)
+
+with tab3:
+    st.subheader("Anime Selection")
+
+    # Selection of an anime in a selectbox
+    selected_anime = st.selectbox("Select an anime:", anime_filtered_df['Name'].unique())
+    
+    # Validation button
+    if st.button("Show Details"):
+        # Filter details of the selected anime
+        anime_details = anime_filtered_df[anime_filtered_df['Name'] == selected_anime]
+        anime_image = anime_df[anime_df['Name'] == selected_anime]['Image URL'].values
+        
+        if not anime_details.empty:
+            st.write("### Details for:", selected_anime)
+            
+            # Column layout for image, details, and pie chart
+            col1, col2, col3 = st.columns([1, 1.5, 1.5])
+            
+            with col1:
+                # Display the image with styling
+                if len(anime_image) > 0:
+                    st.markdown(
+                        f"""
+                        <div style="text-align: center;">
+                            <img src="{anime_image[0]}" style="width: 80%; border-radius: 15px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);" alt="Anime Image">
+                        </div>
+                        """,
+                        unsafe_allow_html=True
+                    )
+                else:
+                    st.write("No image available.")
+            
+            with col2:
+                st.markdown(
+                    f"""
+                    <div style="padding: 10px; font-size: 1.2em; line-height: 1.5;">
+                        <p><strong>Popularity:</strong> {anime_details['Popularity'].values[0]}</p>
+                        <p><strong>Score:</strong> {anime_details['Score'].values[0]}</p>
+                        <p><strong>Members:</strong> {anime_details['Members'].values[0]}</p>
+                        <p><strong>Watching:</strong> {anime_details['Watching'].values[0]}</p>
+                        <p><strong>Completed:</strong> {anime_details['Completed'].values[0]}</p>
+                    </div>
+                    """,
+                    unsafe_allow_html=True
+                )
+
+                # Merge and filter for user scores
+                selected_anime_id = anime_details['anime_id'].values[0]
+                user_scores_for_anime = user_scores_df[user_scores_df['anime_id'] == selected_anime_id]
+                user_scores_merged = user_details_df.merge(user_scores_for_anime, left_on='Mal ID', right_on='user_id', how='inner')
+                user_scores_merged = user_scores_merged[user_scores_merged['Gender'].isin(['Male', 'Female'])]
+
+                if not user_scores_merged.empty:
+                    avg_score_by_gender = user_scores_merged.groupby('Gender')['rating'].mean()
+                    st.markdown("### Average score by gender:")
+                    st.dataframe(avg_score_by_gender.reset_index())
+                    
+            with col3:
+                if not user_scores_merged.empty:
+                    gender_counts = user_scores_merged['Gender'].value_counts()
+                    color_map = {'Male': 'blue', 'Female': 'pink'}
+                    fig_pie = px.pie(
+                        values=gender_counts.values,
+                        names=gender_counts.index,
+                        title=f"Viewer distribution by gender for {selected_anime}",
+                        color=gender_counts.index,
+                        color_discrete_map=color_map
+                    )
+                    st.plotly_chart(fig_pie, use_container_width=True)
+                else:
+                    st.write("No user data available for this anime.")
